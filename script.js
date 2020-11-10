dea11e35a81c926f33b70511446264ded5e3f53e
hussnainjav3d <55350630+hussnainjav3d@users.noreply.github.com>
2020-11-10 23:31:52 +0500

    Adding Full Date on Top

--- script.js
+++ script.js
@@ -6,11 +6,17 @@ function digitalClock() {
     let salutations = document.getElementById('salutations')
     var amPm = 'AM'
     var salutation = 'Good Night'
+    let day = document.getElementById('day')
+    let month = document.getElementById('month')
+    let year = document.getElementById('year')
 
     let h = new Date().getHours()
     let m = new Date().getMinutes()
     let s = new Date().getSeconds()
-
+    let d = new Date().getDate()
+    let mD = new Date().getMonth()
+    let months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUNE', 'JULY', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+    let years = new Date().getFullYear()
 
     if (h < 19 && h > 6) {
         salutation = 'Good Morning'
@@ -29,6 +35,7 @@ function digitalClock() {
     h = h < 10 ? '0' + h : h
     m = m < 10 ? '0' + m : m
     s = s < 10 ? '0' + s : s
+    d = d < 10 ? '0' + d : d
         //  salutations
 
 
@@ -37,6 +44,9 @@ function digitalClock() {
     second.innerHTML = s
     ampm.innerHTML = amPm
     salutations.innerHTML = salutation
+    day.innerHTML = d
+    month.innerHTML = months[mD]
+    year.innerHTML = years
 
 }
 
